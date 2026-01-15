57d48c9f38fc48cc7c3d48022f6db5d714d546e6
non-ee <nonnam.en9@gmail.com>
2026-01-15 14:23:04 +0900

    ...

--- gem5/src/monitor/AccessMonitor.py
+++
@@ -1,9 +0,0 @@
-from m5.params import *
-from MemObject import MemObject
-
-class AccessMonitor(MemObject):
-    type = 'AccessMonitor'
-    cxx_header = "monitor/access_monitor.hh"
-
-    slave = SlavePort("CPU-side port")
-    master = MasterPort("Memory-side port")
